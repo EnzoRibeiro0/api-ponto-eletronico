aab8c17692338bf5f2d1f9190c702a9bf0d0ab6f
Enzo Ribeiro <enzo42490@gmail.com>
2025-07-17 18:27:43 -0300

    feat: Autenticação das rotas de administrador

--- src/main/java/com/pontoeletronico/security/SecurityConfiguration.java
+++ src/main/java/com/pontoeletronico/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.pontoeletronico.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,8 +33,9 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers( HttpMethod.POST, "/auth/login", "/auth/register").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/auth/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()        
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
