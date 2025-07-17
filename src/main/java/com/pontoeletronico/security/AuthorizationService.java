42034f5ba038d35bbeb1cfb1bca07370c1b20c5a
Enzo Ribeiro <enzo42490@gmail.com>
2025-07-17 17:56:33 -0300

    feat: Adição do filtro de ativo/inativo da conta do usuário

--- src/main/java/com/pontoeletronico/security/AuthorizationService.java
+++ src/main/java/com/pontoeletronico/security/AuthorizationService.java
@@ -1,12 +1,17 @@
 package com.pontoeletronico.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.pontoeletronico.exceptions.Exceptions;
+import com.pontoeletronico.model.entity.User;
 import com.pontoeletronico.repository.UserRepository;
 
+@Service
 public class AuthorizationService implements UserDetailsService {
 
     @Autowired
@@ -14,9 +19,14 @@ public class AuthorizationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username);
-    }
-    
 
+        User user = ((User) userRepository.findByEmail(username));
+
+        if (!user.isEnabled()) {
+            throw new Exceptions(HttpStatus.FORBIDDEN, "Usuário inativo");
+        }
+
+        return user;
+    }
 
 }
