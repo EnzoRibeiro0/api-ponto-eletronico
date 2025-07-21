af87d644aae7e9a17b1ef242c892e35a9514fbc2
Enzo Ribeiro <enzo42490@gmail.com>
2025-07-21 18:13:15 -0300

    feat: Criação das funções e rotas de Administrador

--- src/main/java/com/pontoeletronico/controller/AdminRestController.java
+++ src/main/java/com/pontoeletronico/controller/AdminRestController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pontoeletronico.model.dto.RegisterForAdminsDTO;
-import com.pontoeletronico.service.RegisterLoginService;
+import com.pontoeletronico.service.AdminService;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -19,13 +22,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AdminRestController {
  
     @Autowired
-    private RegisterLoginService service;
+    private AdminService service;
 
     @PostMapping("/register-user")
     public ResponseEntity<?> registerNewUser(@RequestBody @Valid RegisterForAdminsDTO dto) {
         
-        return service.registerForAdmins(dto);
+        return service.registerUsers(dto);
     }
     
+    @PutMapping("/inactivate/{id}")
+    public ResponseEntity<?> inactivateUser(@PathVariable String id) {
+        
+        return service.inactivateUser(id);
+    }
 
+    @PutMapping("/activate/{id}")
+    public ResponseEntity<?> activateUser(@PathVariable String id) {
+        
+        return service.activateUser(id);
+    }
 }
---
+++ src/main/java/com/pontoeletronico/service/AdminService.java
@@ -0,0 +1,88 @@
+package com.pontoeletronico.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.pontoeletronico.exceptions.Exceptions;
+import com.pontoeletronico.model.dto.RegisterForAdminsDTO;
+import com.pontoeletronico.model.entity.User;
+import com.pontoeletronico.repository.UserRepository;
+
+@Service
+public class AdminService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+
+    public ResponseEntity<?> registerUsers(RegisterForAdminsDTO dto){
+        
+        if (userRepository.findByEmail(dto.getEmail()) != null) {
+           throw new Exceptions(HttpStatus.CONFLICT, "Usuário já registrado.");
+        }
+
+        User user = new User();
+        
+        String passwordEncripted = new BCryptPasswordEncoder().encode(dto.getPassword());
+        
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncripted);
+        user.setName(dto.getName());
+        user.setRole(dto.getRole());
+        user.setEnable(true);
+        
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuário registrado com sucesso.");
+    }
+
+
+    public ResponseEntity<?> inactivateUser(String id){
+
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado com o id: " + id);
+        }
+        
+        User inactivatedUser = user.get();
+        
+        if (inactivatedUser.isEnabled() == false) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Usuário está inativo com o id: " + id);
+        }
+        
+        inactivatedUser.setEnable(false);
+        userRepository.save(inactivatedUser);
+        
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário inativado com sucesso.");
+
+    }
+
+
+    public ResponseEntity<?> activateUser(String id){
+
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado com o id: " + id);
+        }
+        
+        User activeUser = user.get();
+        
+        if (activeUser.isEnabled() == true) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Usuário está ativo com o id: " + id);
+        }
+
+        activeUser.setEnable(true);
+        userRepository.save(activeUser);
+        
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário ativado com sucesso.");
+
+    }
+
+}
